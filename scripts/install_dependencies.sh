c7905bf3ad9680098de4aba9deb0d79ec776b45e
nari1021 <y.shhh000@gmail.com>
2021-11-04 13:25:56 +0900

    update :: BeforeInstall script 변경

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,10 +2,10 @@
 
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-sudo nvm install node
-sudo node -e "console.log('Running Node.js ' + process.version)"
-sudo nvm install 14
+nvm install node
+node -e "console.log('Running Node.js ' + process.version)"
+nvm install 14
 
 # Install forever module 
 # https://www.npmjs.com/package/forever
-sudo npm install forever -g
+npm install forever -g
